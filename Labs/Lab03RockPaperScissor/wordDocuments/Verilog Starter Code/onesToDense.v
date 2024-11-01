bb422aa6490ae67038f54518800b52274812cac6
Chris Coulston <chris.coulston@gmail.com>
2024-10-31 20:47:12 -0400

    chapter 3 converted
    Converted the figure/table links into Latex references so they show in the lab document.

---
+++ Labs/Lab03RockPaperScissor/wordDocuments/Verilog Starter Code/onesToDense.v
@@ -0,0 +1,18 @@
+//*****************************************************************
+// Name:     
+// Date:     
+// Lab:     03
+// Purp:    playToSeven
+//
+// Assisted: The entire EENG 284 class
+// Assisted by: Dr. Christopher Coulston
+//------------------------------------------------
+//    play[1] = 
+//    play[0] = 
+//*****************************************************************
+module onesToDense (throw, play);
+
+
+
+
+endmodule
---
+++ Labs/Lab03RockPaperScissor/wordDocuments/Verilog Starter Code/rpsGame.v
@@ -0,0 +1,18 @@
+//*****************************************************************
+// Name:    <Your Name>
+// Date:     
+// Lab:     03
+// Purp:    rpsGame
+//
+// Assisted: The entire EENG 284 class
+// Assisted by: Dr. Christopher Coulston
+//------------------------------------------------
+//*****************************************************************
+module rpsGame(p1Throw, p1SevenSeg, p2Throw, p2SevenSeg, playButton, winLoseSeg);
+	
+
+	     
+    onesToDense p1o2d(p1Throw, p1Dense);
+
+
+endmodule
